561d79ca077d35936b6e335881a2ef57ddb8c782
nghiaomg <trungnghia.260716@gmail.com>
2022-09-25 12:25:51 +0700

    js was be long code

--- js/app.js
+++ js/app.js
@@ -3,16 +3,14 @@ function listLoader(){
     $("#note").html("");
     for (let i = 0; i <= window.localStorage.getItem('countNote'); i++) {
         if(window.localStorage.getItem(i) !== null){
+            var itemId = window.localStorage.getItem(i);
             var data = JSON.parse(window.localStorage.getItem(i));
                 $("#note").append(`
                 <a id="my_note" class="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                 <h5 class="text-right font-bold tracking-tight text-gray-900 dark:text-white">
-                    <button type="button" data-toggle="modal" data-target="#modalAccess" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-2 py-2 mr-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-                            <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-                        </svg>
-                    </button>
+                    <span>
+                        <input id="inline-checkbox" type="checkbox" value="${i}" class="hidden w-5 h-5 text-red-600 bg-gray-100 rounded border-gray-300 focus:ring-red-500">
+                    </span>
                     <button type="button" data-toggle="modal" id="info_button_modal" data-target="#exampleModalScrollable" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-2 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
                             <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
@@ -33,7 +31,7 @@ function listLoader(){
                 console.log("hi")
                 loading()
             })
-            
+
             </script>
             `);
         }
